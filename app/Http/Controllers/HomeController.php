b3b4ef8ce3c4762f3e424c35ae0742444fe2f02a
Wellington Gomes Graciani <wellingtongg12@gmail.com>
2019-05-30 19:08:46 -0300

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $registros = Produto::where([
+            'ativo' => 'S'
+            ])->get();
+
+        return view('home', compact('registros'));
+    }
+
+    public function produto($id = null)
+    {
+        if( !empty($id) ) {
+            $registro = Produto::where([
+                'id'    => $id,
+                'ativo' => 'S'
+                ])->first();
+
+            if( !empty($registro) ) {
+                return view('home.produto', compact('registro'));
+            }
+        }
+        return redirect()->route('index');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,83 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="col-md-12">
+            <!--carousel-->
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
+                <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                
+                </ol>
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="images/3.jpg" alt="">
                 </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/4.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/5.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/6.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/7.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/8.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/9.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/10.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="images/11.jpg" alt="">
+                </div>
+                
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Próximo</span>
+                </a>
             </div>
+            <!--carousel-->
         </div>
     </div>
 </div>
 @endsection
+
+<style>
+    body .container {
+        max-width: 100%;
+        padding: 0;
+    }
+
+    body .py-4 {
+        padding-top: 0 !important;
+    }
+
+    body .row {
+        margin-right: 0;
+        margin-left: 0;
+    }
+
+    body .col-md-12 {
+        padding-right: 0;
+        padding-left: 0;
+    }
+</style>
